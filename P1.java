c82c504b4534d4f945a0536b225132516fd56447
nehap3 <npjg0003@gmail.com>
2025-02-22 20:47:43 +0530

    Added first practice code

---
+++ P1.java
@@ -0,0 +1,8 @@
+//Find last Digit of Number
+class Practise{
+    public static void main(String[] args) {
+        int n=231;
+        int lastnum=n%10;
+        System.out.println("Last digit of "+n+" is "+lastnum);
+    }
+}
